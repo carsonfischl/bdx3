74f6e26b815fc0ea00ecb59d2ee0f7142bb3014e
carsonfischl <carson.fischl@carleton.ca>
2024-07-21 16:14:53 +0200

    database work

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import { sql } from "@vercel/postgres";
 
-type Props = {}
+export default async function Form({
+  params
+} : {
+  params: { user: string }
+}): Promise<JSX.Element> {
+  const { rows } = await sql`SELECT * from Pets where owner=${params.user}`;
 
-function form({}: Props) {
   return (
     <div>
-        <h1><strong>STUFF</strong></h1>
-    </div>
-  )
-}
+      <input>
+        <label htmlFor="petName">Pet Name</label>
+        <input type="text" id="petName" name="petName"></input>
 
-export default form
+        <label htmlFor="ownerName">Owner Name</label>
+        <input type="text" id="ownerName" name="ownerName"></input>
+
+        <button type="submit">Submit</button>
+        
+      </input>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,8 @@ export default function Home() {
           priority
         />
       </div>
-      <Form />
+      <div></div>
     </main>
+    
   );
 }
